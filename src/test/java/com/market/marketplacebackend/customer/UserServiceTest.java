f85878af468bdb90585ef0cd57e15698b7b60495
ahn-h-j <tkgkd246@gmail.com>
2025-07-23 12:53:15 +0900

    test : UserService Test 코드
    login_Success_Service
    - userservice.login()을 호출하여 결과 비교
    - customerRepository의 findByEamil() 호출 여부 확인
    
    signUp_Password_Inconsistency_Fail
    - userSerivce의 비밀번호 불일치 시 테스트
    - userSerivce.login에서 IllegalArgumentException이 발생하는지 확인
    - customerRepository의 findByEamil() 호출 여부 확인

--- src/test/java/com/market/marketplacebackend/customer/UserServiceTest.java
+++ src/test/java/com/market/marketplacebackend/customer/UserServiceTest.java
@@ -1,17 +1,20 @@
 package com.market.marketplacebackend.customer;
 
-import com.market.marketplacebackend.common.SuccessDto;
 // Removed unused import for UserController
 import com.market.marketplacebackend.customer.domain.Customer;
+import com.market.marketplacebackend.customer.dto.LoginDto;
 import com.market.marketplacebackend.customer.dto.SignUpDto;
 import com.market.marketplacebackend.customer.repository.CustomerRepository;
 import com.market.marketplacebackend.customer.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -26,6 +29,9 @@ public class UserServiceTest {
 
     @Mock
     private CustomerRepository customerRepository;
+
+    @Mock
+    private HttpSession httpSession;
 
     @Test
     @DisplayName("회원가입 성공(서비스)")
@@ -42,15 +48,20 @@ public class UserServiceTest {
                 .id(1L)
                 .name("test")
                 .email("test@example.com")
+                .password("password123")
+                .phoneNumber("010-1234-5678")
                 .build();
 
         when(customerRepository.save(any(Customer.class)))
                 .thenReturn(customer);
         //when
-        SuccessDto result = userService.join(signUpDto);
+        Customer result = userService.join(signUpDto);
 
         //then
-        assertThat(result.getMessage()).isEqualTo("회원가입 성공");
+        assertThat(result.getEmail()).isEqualTo(signUpDto.getEmail());
+        assertThat(result.getName()).isEqualTo(signUpDto.getName());
+        assertThat(result.getPassword()).isEqualTo(signUpDto.getPassword());
+        assertThat(result.getPhoneNumber()).isEqualTo(signUpDto.getPhoneNumber());
         verify(customerRepository).existsByEmail("test@example.com");
         verify(customerRepository).save(any(Customer.class));
     }
@@ -68,5 +79,59 @@ public class UserServiceTest {
         assertThrows(IllegalArgumentException.class, () -> userService.join(signUpDto));
         //then
         verify(customerRepository).existsByEmail("test@example.com");
+    }
+
+    @Test
+    @DisplayName("로그인 성공(서비스)")
+    void login_Success_Service() throws Exception {
+        //given
+        LoginDto loginDto = LoginDto.builder()
+                .email("test@example.com")
+                .password("password123")
+                .build();
+
+        Customer customer = Customer.builder()
+                .id(1L)
+                .name("test")
+                .email("test@example.com")
+                .password("password123")
+                .phoneNumber("010-1234-5678")
+                .build();
+
+        when(customerRepository.findByEmail("test@example.com")).thenReturn(Optional.ofNullable(customer));
+
+        //when
+        Customer loginCustomer = userService.login(loginDto);
+
+        //then
+        assertThat(loginCustomer.getEmail()).isEqualTo(loginDto.getEmail());
+        assertThat(loginCustomer.getPassword()).isEqualTo(loginDto.getPassword());
+        verify(customerRepository).findByEmail("test@example.com");
+        verify(httpSession).setAttribute("UserId", 1L);
+    }
+
+    @Test
+    @DisplayName("로그인 실패 - 비밀번호 불일치")
+    void signUp_Password_Inconsistency_Fail() throws Exception {
+        //given
+        LoginDto loginDto = LoginDto.builder()
+                .email("test@example.com")
+                .password("invalidPW123")
+                .build();
+
+        Customer customer = Customer.builder()
+                .id(1L)
+                .name("test")
+                .email("test@example.com")
+                .password("password123")
+                .phoneNumber("010-1234-5678")
+                .build();
+
+        when(customerRepository.findByEmail("test@example.com")).thenReturn(Optional.ofNullable(customer));
+
+        //when
+        assertThrows(IllegalArgumentException.class, () -> userService.login(loginDto));
+        //then
+        verify(customerRepository).findByEmail("test@example.com");
     }
 }
