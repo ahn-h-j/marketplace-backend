f9451515744093195114330c9434190176586e58
ahn-h-j <tkgkd246@gmail.com>
2025-07-24 11:17:23 +0900

    feat: 결과 반환 Dto 추가
    ServiceResult
    - success(message, data)
    - 성공 케이스 반환값으로 message와 data를 받아 반환
    - failure
    - 실패 케이스 반환값으로 errorCode와 message를 받아 반환
    
    ErrorCode
    - 실패 케이스에 대한 ErrorCode Enum
    
    UserController
    - 반환값 변경
    - ApiResponse -> ServiceResult
    
    UserService
    - 반환값 변경
    - ApiResponse -> ServiceResult
    - 실패 케이스 처리 방식 변경
    - 예외 발생 -> ServiceResult.failure 반환
    
    ApiResponse
    - ServiceResult로 대체

---
+++ src/main/java/com/market/marketplacebackend/common/ErrorCode.java
@@ -0,0 +1,14 @@
+package com.market.marketplacebackend.common;
+
+// ErrorCode.java
+
+public class ErrorCode {
+
+    // 회원가입 관련
+    public static final String EMAIL_DUPLICATE = "EMAIL_DUPLICATE";
+
+    // 로그인 관련
+    public static final String EMAIL_NOT_FOUND = "EMAIL_NOT_FOUND";
+    public static final String PASSWORD_MISMATCH = "PASSWORD_MISMATCH";
+
+}
