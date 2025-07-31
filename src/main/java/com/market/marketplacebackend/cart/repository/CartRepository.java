d33f1c2e9f3b3114cd80eebae53e9f439d9632ae
ahn-h-j <tkgkd246@gmail.com>
2025-07-31 14:58:54 +0900

    fix : Cart 조회 방법 수정 findById(accountId) -> findByAccountId(accountId)

--- src/main/java/com/market/marketplacebackend/cart/repository/CartRepository.java
+++ src/main/java/com/market/marketplacebackend/cart/repository/CartRepository.java
@@ -3,5 +3,8 @@ package com.market.marketplacebackend.cart.repository;
 import com.market.marketplacebackend.cart.domain.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    Optional<Cart> findByAccountId(Long accountId);
 }
