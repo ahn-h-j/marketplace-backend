b90ec60488e570818b193b679618796b97369029
ahn-h-j <tkgkd246@gmail.com>
2025-07-31 10:37:30 +0900

    feat : Cart 도메인 추가
    Cart
    - 장바구니 관련 Entity
    - 사용자, 장바구니 상품 필드를 가짐
    - Account와 1 : 1 관계를 가짐
    - CartItem과 N : 1 관계를 가짐
    
    CartItem
    - 장바구니 내 상품 관련 Entity
    - 상품, 카트, 수량 필드를 가짐
    - Product와 1 : N 관계를 가짐
    - Cart와 1 : N 관계를 가짐

---
+++ src/main/java/com/market/marketplacebackend/cart/domain/Cart.java
@@ -0,0 +1,33 @@
+package com.market.marketplacebackend.cart.domain;
+
+import com.market.marketplacebackend.account.domain.Account;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> cartItems = new ArrayList<>();
+
+    public Cart(Account account) {
+        this.account = account;
+    }
+}
---
+++ src/main/java/com/market/marketplacebackend/cart/domain/CartItem.java
@@ -0,0 +1,31 @@
+package com.market.marketplacebackend.cart.domain;
+
+import com.market.marketplacebackend.product.domain.Product;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @Column(nullable = false)
+    private int quantity;
+}
