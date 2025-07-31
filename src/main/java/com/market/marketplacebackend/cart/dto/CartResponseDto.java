12171e8453da4e31e0ca345035671bcbf680b3a3
ahn-h-j <tkgkd246@gmail.com>
2025-07-31 14:59:48 +0900

    fix : List변경
    List<CartItem>은 엔티티를 그대로 반환하기에 List<CartItemResponseDto>로 변경

--- src/main/java/com/market/marketplacebackend/cart/dto/CartResponseDto.java
+++ src/main/java/com/market/marketplacebackend/cart/dto/CartResponseDto.java
@@ -1,25 +1,29 @@
-package com.market.marketplacebackend.cart.controller;
+package com.market.marketplacebackend.cart.dto;
 
 import com.market.marketplacebackend.cart.domain.Cart;
-import com.market.marketplacebackend.cart.domain.CartItem;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Builder
 public class CartResponseDto {
-    private List<CartItem> items;
+    private List<CartItemResponseDto> items;
     private int totalPrice;
 
     public static CartResponseDto from(Cart cart) {
+        List<CartItemResponseDto> itemDtos = cart.getCartItems().stream()
+                .map(CartItemResponseDto::fromEntity)
+                .collect(Collectors.toList());
+
         int total = cart.getCartItems().stream()
                 .mapToInt(item -> item.getProduct().getPrice() * item.getQuantity())
                 .sum();
 
         return CartResponseDto.builder()
-                .items(cart.getCartItems())
+                .items(itemDtos)
                 .totalPrice(total)
                 .build();
     }
