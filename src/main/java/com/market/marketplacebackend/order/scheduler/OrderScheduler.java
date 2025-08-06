6fda928db16ca7da18e2063083cbd8a79427c382
ahn-h-j <tkgkd246@gmail.com>
2025-08-06 22:45:21 +0900

    fix : Batch 크기가 커지면 Transaction이 길어지는 문제가 발생
    Page를 사용하여 일정 단위마다 Transaction을 나눠 Transaction 크기를 줄임
    
    또한 OrderCancellationService의 cancelOrder메서드에 Propagation.REQUIRES_NEW를 사용하여 해당 메서드가 실행될때 마다 새로운 Transaction을 생성하도록 만듬

--- src/main/java/com/market/marketplacebackend/order/scheduler/OrderScheduler.java
+++ src/main/java/com/market/marketplacebackend/order/scheduler/OrderScheduler.java
@@ -3,34 +3,40 @@ package com.market.marketplacebackend.order.scheduler;
 import com.market.marketplacebackend.common.enums.OrderStatus;
 import com.market.marketplacebackend.order.domain.Order;
 import com.market.marketplacebackend.order.repository.OrderRepository;
-import com.market.marketplacebackend.product.domain.Product;
+import com.market.marketplacebackend.order.service.OrderCancellationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class OrderScheduler {
 
     private final OrderRepository orderRepository;
+    private static final int BATCH_SIZE = 100;
+    private final OrderCancellationService orderCancellationService;
 
     @Scheduled(fixedDelay = 60000)
     @Transactional
     public void cleanupExpiredOrders(){
-        List<Order> expiredOrders = orderRepository.findByOrderStatusAndExpiresAtBefore(OrderStatus.PENDING, LocalDateTime.now());
+        Pageable pageable = PageRequest.of(0, BATCH_SIZE);
+        Page<Order> expiredOrdersPage;
+        do{
+            expiredOrdersPage = orderRepository.findExpiredOrdersWithDetails(OrderStatus.PENDING, LocalDateTime.now(), pageable);
 
-        for (Order expiredOrder : expiredOrders) {
-            expiredOrder.cancel();
-            expiredOrder.getOrderItems().forEach(
-                    orderItem -> {
-                        Product product = orderItem.getCartItem().getProduct();
-                        product.increaseStock(orderItem.getQuantity());
-                    });
-        }
-
+            if(!expiredOrdersPage.hasContent()){
+                break;
+            }
+            for (Order expiredOrder : expiredOrdersPage.getContent()) {
+               orderCancellationService.cancelOrder(expiredOrder);
+            }
+            pageable = expiredOrdersPage.nextPageable();
+        }while (expiredOrdersPage.hasNext());
     }
 }
