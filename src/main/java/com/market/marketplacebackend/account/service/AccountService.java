bceb1cb5035a8b43155238f668b5afcfe844ea46
ahn-h-j <tkgkd246@gmail.com>
2025-08-22 19:17:45 +0900

    fix : 로그인 방식 변경
    AccountController
    - 엔드포인트 (Post /login) 삭제
    - JWT 방식으로 대체
    
    AccountService
    - 비밀번호 암호화 추가
    - login 메서드 삭제
    
    SignUpDto
    - toEntity 암호화된 비밀번호를 파라미터로 받아 처리

--- src/main/java/com/market/marketplacebackend/account/service/AccountService.java
+++ src/main/java/com/market/marketplacebackend/account/service/AccountService.java
@@ -4,11 +4,11 @@ import com.market.marketplacebackend.cart.service.CartService;
 import com.market.marketplacebackend.common.exception.BusinessException;
 import com.market.marketplacebackend.common.exception.ErrorCode;
 import com.market.marketplacebackend.account.domain.Account;
-import com.market.marketplacebackend.account.dto.LoginDto;
 import com.market.marketplacebackend.account.dto.SignUpDto;
 import com.market.marketplacebackend.account.repository.AccountRepository;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,32 +16,18 @@ import org.springframework.stereotype.Service;
 public class AccountService {
 
     private final AccountRepository accountRepository;
-    private final HttpSession httpSession;
     private final CartService cartService;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     public Account join(SignUpDto signUpDto) {
         if(accountRepository.existsByEmail(signUpDto.getEmail())){
             throw new BusinessException(ErrorCode.EMAIL_DUPLICATE);
         }
-
-        Account account = signUpDto.toEntity();
+        String password = passwordEncoder.encode(signUpDto.getPassword());
+        Account account = signUpDto.toEntity(password);
         Account savedAccount = accountRepository.save(account);
         cartService.createCartForAccount(savedAccount);
 
         return savedAccount;
     }
-
-    public Account login(LoginDto loginDto) {
-        Account account = accountRepository.findByEmail(loginDto.getEmail())
-                .orElseThrow(() -> new BusinessException(ErrorCode.EMAIL_NOT_FOUND));
-
-        if(!account.getPassword().equals(loginDto.getPassword())){
-            throw new BusinessException(ErrorCode.PASSWORD_MISMATCH);
-        }
-
-        httpSession.setAttribute("UserId", account.getId());
-
-        return account;
-    }
-
 }
