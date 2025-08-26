6f093173f04f2f7b6896fbf02be42d886d078f26
ahn-h-j <tkgkd246@gmail.com>
2025-08-26 11:19:05 +0900

    4. feat: CustomOAuth2UserService OAuth 사용자 처리 서비스 구현
    CustomOAuth2UserService
    - DefaultOAuth2UserService 상속으로 OAuth 사용자 정보 처리
    - ProviderType.from으로 제공자별 OAuth2UserInfo 생성
    - accountRepository.findByEmail로 기존 계정 조회
    - 신규 사용자 시 AccountRole.BUYER 기본값으로 Account 생성
    - PrincipalDetails 반환
    
    PrincipalDetails
    - UserDetails와 OAuth2User 인터페이스 동시 구현
    - OAuth 로그인과 일반 로그인 통합 지원
    - getName에서 providerId 우선 반환, 없으면 email 사용

---
+++ src/main/java/com/market/marketplacebackend/security/service/CustomOAuth2UserService.java
@@ -0,0 +1,57 @@
+package com.market.marketplacebackend.security.service;
+
+
+import com.market.marketplacebackend.account.domain.Account;
+import com.market.marketplacebackend.account.repository.AccountRepository;
+import com.market.marketplacebackend.common.enums.AccountRole;
+import com.market.marketplacebackend.common.enums.ProviderType;
+import com.market.marketplacebackend.security.domain.PrincipalDetails;
+import com.market.marketplacebackend.security.provider.OAuth2UserInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    private final AccountRepository accountRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+
+        OAuth2UserInfo oAuth2UserInfo = ProviderType.from(registrationId).createUserInfo(attributes);
+
+        String provider = oAuth2UserInfo.getProvider();
+        String providerId = oAuth2UserInfo.getProviderId();
+        String name = oAuth2UserInfo.getName();
+        String email = oAuth2UserInfo.getEmail();
+
+        Optional<Account> account = accountRepository.findByEmail(email);
+
+        Account userAccount;
+        if(account.isPresent()){
+            userAccount = account.get();
+        }else{
+            Account newAccount = Account.builder()
+                    .name(name)
+                    .email(email)
+                    .accountRole(AccountRole.BUYER)
+                    .provider(provider)
+                    .providerId(providerId)
+                    .build();
+            userAccount = accountRepository.save(newAccount);
+        }
+        return new PrincipalDetails(userAccount, attributes);
+
+    }
+}
+
