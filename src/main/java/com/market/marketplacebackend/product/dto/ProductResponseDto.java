be896855327f97a79c2f7c4ce4fadaa03b4f76df
ahn-h-j <tkgkd246@gmail.com>
2025-09-03 16:23:59 +0900

    feat : Dto에 imageUrl 추가

--- src/main/java/com/market/marketplacebackend/product/dto/ProductResponseDto.java
+++ src/main/java/com/market/marketplacebackend/product/dto/ProductResponseDto.java
@@ -16,13 +16,15 @@ public class ProductResponseDto {
     private Integer price;
     private Category category;
     private String sellerName;
+    private String imageUrl;
 
     public static ProductResponseDto fromEntity(Product product) {
         return ProductResponseDto.builder()
                 .name(product.getName())
                 .price(product.getPrice())
                 .category(product.getCategory())
-                .sellerName(product.getAccount().getName()) // 필요한 정보만 가공
+                .sellerName(product.getAccount().getName())
+                .imageUrl(product.getImageUrl())
                 .build();
     }
 }
